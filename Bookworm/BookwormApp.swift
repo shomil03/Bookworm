403043c0c7bf94bb1f1cb99691f8671c3c356ed0
shomil03 <shomil02020@gmail.com>
2024-03-17 17:57:01 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Shomil Singh on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
